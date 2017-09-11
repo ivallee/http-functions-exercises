9912725082de6f841cea1d74002cdb3c34d44321
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 21:37:31 +0000

    add reverse function

--- callbacks/print-lowercase.js
+++ callbacks/print-lowercase.js
@@ -2,7 +2,7 @@ const getHTML = require('./http-functions');
 
 const requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step6/lowercasegit .html'
+  path: '/http-examples/step6/lowercase.html'
 };
 
 
---
+++ callbacks/print-reverse.js
@@ -0,0 +1,13 @@
+const getHTML = require('./http-functions');
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+
+function printReverse(html) {
+  console.log(html.split('').reverse().join(''));
+}
+
+getHTML(requestOptions, printReverse);
