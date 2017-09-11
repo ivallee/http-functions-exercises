8e772d37efab620655442381c7785a9c8526dbd5
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 19:23:42 +0000

    made function more modular

---
+++ step3/step3.js
@@ -0,0 +1,27 @@
+const https = require('https');
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+function getAndPrintHTML(options) {
+
+
+  https.get(options, (res) => {
+    var content = '';
+    res.setEncoding('utf8');
+
+    res.on('data', (data) => {
+      content += data;
+    });
+
+    res.on('end', () => {
+      console.log(content);
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+module.exports = getAndPrintHTML;
+console.log(getAndPrintHTML(requestOptions));
