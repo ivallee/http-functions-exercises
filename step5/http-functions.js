f92b2ad4a945b7bc5cf9b522c253c226b33ab5e7
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 19:45:58 +0000

    create http-functions file

---
+++ step5/http-functions.js
@@ -0,0 +1,31 @@
+const https = require('https');
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+function printHTML (html) {
+  console.log(html);
+}
+
+function getHTML (options, callback) {
+
+  https.get(options, (res) => {
+    var content = '';
+    res.setEncoding('utf8');
+
+    res.on('data', (data) => {
+      content += data;
+    });
+
+    res.on('end', () => {
+      printHTML(content);
+      console.log('Response stream complete.');
+    });
+  });
+
+}
+
+module.exports = getHTML;
+getHTML(requestOptions, printHTML);
