748c728f19a6fc63e4d4751562195286b1dd1c6c
Isaac Vallee <isaacvallee@gmail.com>
2017-09-11 18:20:46 +0000

    completed and linted print chunks function

--- print-chunks/print-chunks.js
+++ print-chunks/print-chunks.js
@@ -1,30 +1,23 @@
-// while https is built-in to Node, it is a module, so it must be required
 const https = require('https');
 
-// the host can be thought of as the domain name you want to read from,
-// and the path is the resource - '/' is the root path, but if you wanted to read a
-// particular resource (like '/login/index.html'), that would be defined in the path
-const requestOptions = {
-  host: 'example.com',
-  path: '/'
-};
+function getAndPrintHTMLChunks () {
 
-// notice that https.get takes a callback with one parameter -
-// response, which is a Stream that represents the HTTP response
-https.get(requestOptions, (res) => {
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
 
-  // set encoding of received data to UTF-8
-  response.setEncoding('utf8');
+  https.get(requestOptions, (res) => {
 
-  // the callback is invoked when a `data` chunk is received
-  response.on('data', (data) => {
-    console.log('Chunk Received. Length:', data.length);
+    res.setEncoding('utf8');
+
+    res.on('data', (data) => {
+      console.log(data + '\n');
+    });
+
+    res.on('end', () => {
+      console.log('Response stream complete.');
+    });
   });
-
-  // the callback is invoked when all of the data has been received
-  // (the `end` of the stream)
-  response.on('end', () => {
-    console.log('Response stream complete.');
-  });
-
-});
+}
+module.exports = getAndPrintHTMLChunks;
